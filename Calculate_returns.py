a42140ada0d6d0aa2d1d66580e13e2f7bc1f236a
himanshu2oo4 <86143385+himanshu2oo4@users.noreply.github.com>
2024-05-23 19:26:50 +0530

    Update Calculate_returns.py

--- Calculate_returns.py
+++ Calculate_returns.py
@@ -176,7 +176,7 @@ def app():
 
         # new -----------
         st.markdown('''
-    <div style="display: flex; justify-content: space-around;">
+    <div style="display: flex-box; justify-content: space-around;">
         <h3>Wanna buy any of these stocks 💹💸 ? </h3> <br>
         <a href="https://groww.in/" target="_blank">
             <button type="button">On Groww</button>
